c75ac1c01ad950e7acd13edb89f70fc3da501392
anthozb3x <anthony.beal@clsi.fr>
2024-03-04 21:50:30 +0100

    fix : fix testEstDansPLageTrue et testEstDansPLageFalse

--- src/test/java/com/ipi/jva350/model/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/model/EntrepriseTest.java
@@ -6,15 +6,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import com.ipi.jva350.model.Entreprise;
 
 public class EntrepriseTest {
     @Test
     public void testEstDansPlageTrue() {
 
         // GIVEN
-            Entreprise entreprise = new Entreprise();
+
         // WHEN
-            boolean res = entreprise.estDansPlage(LocalDate.now, LocalDate.of(2024,02,18), LocalDate(2024,04,10));
+            boolean res = Entreprise.estDansPlage(LocalDate.now(), LocalDate.of(2024,02,18), LocalDate.of(2024,04,10));
         // THEN
 
         Assertions.assertEquals(true, res, "La date indiquée est bien dans la plage selectionnée");
@@ -24,9 +25,9 @@ public class EntrepriseTest {
     public void testEstDansPlageFalse() {
 
         // GIVEN
-        Entreprise entreprise = new Entreprise();
+
         // WHEN
-        boolean res = entreprise.estDansPlage(LocalDate.now, LocalDate.of(2012,02,18), LocalDate(2023,04,10));
+        boolean res = Entreprise.estDansPlage(LocalDate.now(), LocalDate.of(2012,02,18), LocalDate.of(2023,04,10));
         // THEN
 
         Assertions.assertEquals(false, res, "La date indiquée n'est pas dans la plage selectionnée");
